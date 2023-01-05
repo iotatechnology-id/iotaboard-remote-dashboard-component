bf5f3f0ddeea079629826df9bbb4c9996e54ee7f
Seiko Santana <santanaaseiko@gmail.com>
2023-01-05 13:50:30 +0700

    Add CSS Loader

--- webpack-demo.config.js
+++ webpack-demo.config.js
@@ -27,6 +27,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader'],
       }
     ]
   },
