0afa9ef4ccbe64042bdd6673420398e6a9164e88
Seiko Santana <santanaaseiko@gmail.com>
2023-01-05 15:25:49 +0700

    Add Sample Chart

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,3 +1,18 @@
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
-export const Title = ({ children }) => <h1>{children}</h1>;
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const Title = ({ children }) => <div>
+    <h1>{children}</h1>
+    <Doughnut data={{
+        datasets: [
+            {
+                data: [1, 2, 6, 4],
+                borderColor: "#ffff00",
+                backgroundColor: "#ff0000"
+            }
+        ]
+    }}></Doughnut>
+</div>
