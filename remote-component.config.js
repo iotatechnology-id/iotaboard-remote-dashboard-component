b7a5a5b860b4c61f190952c86535f1b39ef84a07
seikosantana <santanaaseiko@gmail.com>
2023-03-16 16:32:25 +0700

    Backport dependency sharing

--- remote-component.config.js
+++ remote-component.config.js
@@ -4,6 +4,21 @@
 
 module.exports = {
   resolve: {
-    react: require("react")
+    "react": "react",
+    "@ionic/react": "@ionic/react",
+    "@ionic/react-router": "@ionic/react-router",
+    "@microsoft/signalr": "@microsoft/signalr",
+    "@types/react": "@types/react",
+    "@types/react-dom": "@types/react-dom",
+    "@types/react-router": "@types/react-router",
+    "@types/react-router-dom": "@types/react-router-dom",
+    "react-router-dom": "react-router-dom",
+    "chart.js": "chart.js",
+    "date-fns": "date-fns",
+    "ionicons": "ionicons",
+    "react-chartjs-2": "react-chartjs-2",
+    "react-dom": "react-dom",
+    "react-router": "react-router",
+    "react-router-dom": "react-router-dom"
   }
 };
