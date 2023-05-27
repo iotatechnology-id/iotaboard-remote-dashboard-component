ff8794add7b887db498f8ebe9f6652ff791f4737
seikosantana <santanaaseiko@gmail.com>
2023-05-27 14:24:33 +0700

    Add support for asset background init

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,17 +3,33 @@ import type {
   RemoteDashboardBackgroundInitFn
 } from "../../../../src/services/remote-dashboard-interop";
 const backgroundInit: RemoteDashboardBackgroundInitFn = async (
-  dashboardId: string,
-  interops: RegisteredInterops
+  interops: RegisteredInterops,
+  dashboardId?: string,
+  assetId?: string
 ) => {
-  const response = await interops.defaultIotaboardClient.getDashboard(dashboardId);
-  if (response.statusCode == 200) {
-    const dashboard = response.data;
-    if (!dashboard) {
-      return;
+  // TODO: Implement background functionality or remove if unecessary
+  if (dashboardId) {
+    const response = await interops.defaultIotaboardClient.getDashboard(dashboardId);
+    if (response.statusCode == 200) {
+      const dashboard = response.data;
+      if (!dashboard) {
+        return;
+      }
+      const settings = dashboard.settings;
+      // Do something in the background
     }
-    const settings = dashboard.settings;
-    // Do something in the background
+  }
+  if (assetId) {
+    const response = await interops.defaultIotaboardClient.getThisUserAssetAccessPermission(assetId);
+    if (response.statusCode == 200) {
+      const permission = response.data;
+      if (!permission) {
+        return;
+      }
+      
+      // Do something in the background
+      // consume permission
+    }
   }
 };
 
